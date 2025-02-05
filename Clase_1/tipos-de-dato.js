4d8625d480da38ecae395578476fb5c9d30ecd6f
Akeri <samaescii@gmail.com>
2025-02-04 20:39:45 -0600

    Actividad en clase

---
+++ Clase_1/tipos-de-dato.js
@@ -0,0 +1,45 @@
+//Este es un comentario en JavaScript
+//console.log se usa para imprimir en consola 
+console.log("Hola mundo");
+
+var nombre = "Akeri"; //String = Cadena
+var edad = 21; // Number, puede ser entero o decimal
+var edad_decimal = 21.05;
+var estudiante = true; //Boolean
+var superman = false; //Boolean
+var peces = null; //Null
+var datos; //Undefined
+
+//Ingresa "typeof" en col¿nsola, seguido de la variable que quieres consultar.
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(edad_decimal));
+console.log(typeof(estudiante));
+console.log(typeof(superman));
+console.log(typeof(peces));
+console.log(typeof(datos));
+
+/* Inicio
+Comentario en bloque 
+Texto
+Texto
+Cierre */ 
+
+
+//ACTIVIDAD 
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(verdadero));
+console.log(typeof(nulo));
+// Error -> console.log(typeof("));
+console.log(typeof(indefinido));
+console.log(typeof(FALSO));
+
+var deporte = "MuayThai";
+var num = 7;
+var aprendiendo = true;
+var durmiendo = false;
+var galletas = null;
+var pastel; 
